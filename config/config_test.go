44dfa4a9fe1791a172beff2b75156b63476bf561
zhongxic <2054939673@qq.com>
2024-11-11 23:23:10 +0800

    单元测试非致命错误使用 t.Error 报告而不是 t.Fatal

--- config/config_test.go
+++ config/config_test.go
@@ -15,15 +15,15 @@ func TestParse(t *testing.T) {
 		t.Fatal(err)
 	}
 	if config.Logging.Level != expectedLogLevel {
-		t.Fatalf("expected log level [%v] actual [%v]", expectedLogLevel, config.Logging.Level)
+		t.Errorf("expected log level [%v] actual [%v]", expectedLogLevel, config.Logging.Level)
 	}
 	if config.Logging.File != expectedLogFile {
-		t.Fatalf("expected log file [%v] actual [%v]", expectedLogFile, config.Logging.File)
+		t.Errorf("expected log file [%v] actual [%v]", expectedLogFile, config.Logging.File)
 	}
 	if config.Logging.MaxAge != expectedLogFileAge {
-		t.Fatalf("expected log file age [%v] actual [%v]", expectedLogFile, config.Logging.MaxAge)
+		t.Errorf("expected log file age [%v] actual [%v]", expectedLogFile, config.Logging.MaxAge)
 	}
 	if config.Logging.MaxSize != expectedLogFileSize {
-		t.Fatalf("expected log file size [%v] actual [%v]", expectedLogFile, config.Logging.MaxSize)
+		t.Errorf("expected log file size [%v] actual [%v]", expectedLogFile, config.Logging.MaxSize)
 	}
 }
