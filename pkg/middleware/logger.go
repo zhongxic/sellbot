40a9613ce513b0f47f7a0eeaa3132dbae6d567c0
zhongxic <2054939673@qq.com>
2024-11-20 23:25:31 +0800

    优化 logger 中间件

--- pkg/middleware/logger.go
+++ pkg/middleware/logger.go
@@ -2,21 +2,14 @@ package middleware
 
 import (
 	"bytes"
-	"encoding/json"
 	"io"
 	"log/slog"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/zhongxic/sellbot/pkg/errorcode"
 	"github.com/zhongxic/sellbot/pkg/model"
-)
-
-const (
-	contentType     = "Content-Type"
-	applicationJson = "application/json"
 )
 
 type ResponseWriterWrapper struct {
@@ -43,11 +36,12 @@ func Logger() gin.HandlerFunc {
 		start := time.Now()
 		path := c.Request.URL.Path
 		query := c.Request.URL.RawQuery
-		body, err := dumpRequest(c)
+		body, err := c.GetRawData()
 		if err != nil {
-			result := model.FailedWithCode(errorcode.SystemError, "dump request failed")
-			c.AbortWithStatusJSON(http.StatusInternalServerError, result)
+			c.AbortWithStatusJSON(http.StatusInternalServerError,
+				model.FailedWithCode(errorcode.SystemError, "dump request failed"))
 		}
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
 
 		writer := &ResponseWriterWrapper{c.Writer, &bytes.Buffer{}}
 		c.Writer = writer
@@ -56,48 +50,16 @@ func Logger() gin.HandlerFunc {
 
 		status := c.Writer.Status()
 		elapsed := time.Since(start).Milliseconds()
-		response := dumpResponse(writer, c)
+		response, _ := io.ReadAll(writer.Body)
 
 		slog.Info("completed",
 			slog.Group("request",
 				slog.String("path", path),
 				slog.String("query", query),
-				slog.Any("body", body),
+				slog.String("body", string(body)),
 				slog.Int64("elapsed", elapsed)),
 			slog.Group("response",
 				slog.Int64("status", int64(status)),
-				slog.Any("body", response)))
+				slog.String("body", string(response))))
 	}
-}
-
-func dumpRequest(c *gin.Context) (body *map[string]any, err error) {
-	if c.Request.Header.Get(contentType) == applicationJson {
-		if data, err := io.ReadAll(c.Request.Body); err != nil {
-			return nil, err
-		} else {
-			m := &map[string]any{}
-			if err := json.Unmarshal(data, m); err != nil {
-				return nil, err
-			}
-			c.Request.Body = io.NopCloser(bytes.NewBuffer(data))
-			return m, nil
-		}
-	}
-	return &map[string]any{}, nil
-}
-
-func dumpResponse(writer *ResponseWriterWrapper, c *gin.Context) (body *map[string]any) {
-	m := &map[string]any{"addition": "not in json format or dump failed"}
-	if strings.Contains(c.Writer.Header().Get(contentType), applicationJson) {
-		if data, err := io.ReadAll(writer.Body); err != nil {
-			return m
-		} else {
-			res := &map[string]any{}
-			if err := json.Unmarshal(data, res); err != nil {
-				return m
-			}
-			return res
-		}
-	}
-	return m
 }
