60e83858130b7df1305f8752197e122a173280eb
zhongxic <2054939673@qq.com>
2024-11-24 15:04:50 +0800

    实现按照正则表达式分割字符串且返回所有组成部分而不只是匹配表达式的部分

---
+++ pkg/jieba/regex.go
@@ -0,0 +1,23 @@
+package jieba
+
+import "regexp"
+
+// Split slices s into substrings separated by the regex and returns a slice of substrings.
+//
+// The result contains all part of s, not just the part matched regex.
+func Split(s string, re *regexp.Regexp) []string {
+	var result []string
+	index := 0
+	indices := re.FindAllStringIndex(s, -1)
+	for _, pair := range indices {
+		if index < pair[0] {
+			result = append(result, s[index:pair[0]])
+		}
+		result = append(result, s[pair[0]:pair[1]])
+		index = pair[1]
+	}
+	if index < len(s) {
+		result = append(result, s[index:])
+	}
+	return result
+}
---
+++ pkg/jieba/regex_test.go
@@ -0,0 +1,17 @@
+package jieba
+
+import (
+	"regexp"
+	"strings"
+	"testing"
+)
+
+func TestSplit(t *testing.T) {
+	s := "这是一个伸手不见五指的黑夜。我叫孙悟空，我爱北京，我爱Python和C++。"
+	re := regexp.MustCompile(`([\p{Han}a-zA-Z0-9+#&._%\-]+)`)
+	splits := Split(s, re)
+	joined := strings.Join(splits, "")
+	if joined != s {
+		t.Errorf("expected s after joined [%v] actual [%v]", s, joined)
+	}
+}
