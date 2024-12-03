cba6972589fed909fb32cc2c22d4baf54cb03f6d
zhongxic <2054939673@qq.com>
2024-12-03 22:57:24 +0800

    修改 recover 中间件的日志

--- pkg/middleware/recover.go
+++ pkg/middleware/recover.go
@@ -13,7 +13,7 @@ func Recover() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
-				slog.Error("error recovered", slog.Any("error", err))
+				slog.Error("recovered", "error", err)
 				c.AbortWithStatusJSON(http.StatusInternalServerError,
 					result.FailedWithErrorCode(errorcode.SystemError, "internal server error"))
 			}
