413e4e119d95f4c9ec03b374f0663e4e8d5d238b
zhongxic <2054939673@qq.com>
2024-12-23 09:26:50 +0800

    会话增加节点分支命中次数统计

--- internal/service/session/session.go
+++ internal/service/session/session.go
@@ -11,8 +11,23 @@ type Session struct {
 	CurrentBranch         string
 	LastMainProcessDomain string
 	LastMainProcessBranch string
+	DomainBranchHitCount  map[string]map[string]int
 }
 
 func New() *Session {
-	return &Session{SessionId: uuid.New().String()}
+	return &Session{
+		SessionId:            uuid.New().String(),
+		DomainBranchHitCount: make(map[string]map[string]int),
+	}
+}
+
+func (s *Session) GetDomainBranchHitCount(domainName, branchName string) int {
+	if len(s.DomainBranchHitCount) == 0 {
+		return 0
+	}
+	branchHitCount, ok := s.DomainBranchHitCount[domainName]
+	if !ok || len(branchHitCount) == 0 {
+		return 0
+	}
+	return branchHitCount[branchName]
 }
