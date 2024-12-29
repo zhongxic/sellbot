0c4d057090275c99acc8eeb8fa8c73ed114ed156
zhongxic <2054939673@qq.com>
2024-12-29 21:21:24 +0800

    修改 matcher 接口的定义

--- internal/service/bot/matcher/matcher.go
+++ internal/service/bot/matcher/matcher.go
@@ -1,7 +1,11 @@
 package matcher
 
+import (
+	"context"
+)
+
 type Matcher interface {
-	// Match find matched branches in process use this context
+	// Match find matched branches in process use this matchContext
 	// and abort match if return false
-	Match(matchContext *Context) bool
+	Match(ctx context.Context, matchContext *Context) (abort bool, err error)
 }
