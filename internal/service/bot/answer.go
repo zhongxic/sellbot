c048203713d7c292ff036e74622fba3bf583ffe4
zhongxic <2054939673@qq.com>
2025-01-03 20:19:02 +0800

    增加流程跳转次限制数防止出现死循环

--- internal/service/bot/answer.go
+++ internal/service/bot/answer.go
@@ -11,6 +11,12 @@ import (
 	"github.com/zhongxic/sellbot/internal/traceid"
 )
 
+var maxAllowedJumpCount = 5
+
+// autoJumpCountContextKey is a context key to retrieve auto jumped count in current process.
+type autoJumpCountContextKey struct {
+}
+
 func makeAnswer(ctx context.Context, matchContext *matcher.Context) (AnswerDTO, error) {
 	if matchContext == nil {
 		return AnswerDTO{}, fmt.Errorf("make answer failed due to nil match context")
@@ -39,7 +45,6 @@ func makeAnswer(ctx context.Context, matchContext *matcher.Context) (AnswerDTO, 
 	isExceed := hitCount >= len(branch.Responses) && domain.Category != process.DomainCategoryMainProcess
 	slog.Info(fmt.Sprintf("sessionId [%v]: domain [%v] branch [%v] hitCount [%v] isExceed [%v]",
 		matchContext.Session.Id, matchedPath.Domain, matchedPath.Branch, hitCount, isExceed), traceId)
-	// TODO loop forever when session going to end domain but continue interactive
 	if isExceed {
 		nextDomain := ""
 		if branch.EnableExceedJump && branch.Next != "" {
@@ -66,8 +71,19 @@ func makeAnswer(ctx context.Context, matchContext *matcher.Context) (AnswerDTO, 
 }
 
 func autoJump(ctx context.Context, matchContext *matcher.Context, nextDomain string) (AnswerDTO, error) {
-	var matchedPath matcher.MatchedPath
 	processHelper := process.NewHelper(matchContext.Process)
+	// make sure not loop forever
+	autoJumpCount := ctx.Value(autoJumpCountContextKey{})
+	if autoJumpCount != nil && autoJumpCount.(int) >= maxAllowedJumpCount {
+		return AnswerDTO{}, fmt.Errorf("max allowed jump count exceed")
+	}
+	var jumpContext context.Context
+	if autoJumpCount == nil {
+		jumpContext = context.WithValue(ctx, autoJumpCountContextKey{}, 0)
+	} else {
+		jumpContext = context.WithValue(ctx, autoJumpCountContextKey{}, autoJumpCount.(int)+1)
+	}
+	var matchedPath matcher.MatchedPath
 	if nextDomain == "" {
 		domain, err := processHelper.GetCommonDialog(process.DomainTypeDialogEndFail)
 		if err != nil {
@@ -83,5 +99,5 @@ func autoJump(ctx context.Context, matchContext *matcher.Context, nextDomain str
 		matchContext.Session.Id, nextDomain, matchedPath.Domain, matchedPath.Branch),
 		slog.Any("traceId", ctx.Value(traceid.TraceId{})))
 	matchContext.AddMatchedPath(matchedPath)
-	return makeAnswer(ctx, matchContext)
+	return makeAnswer(jumpContext, matchContext)
 }
